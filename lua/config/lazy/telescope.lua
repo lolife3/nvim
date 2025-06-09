e98e61d066104771d8061eaa88c03e7bfc2a0f1e
lolife3 <makara.biznes@gmail.com>
2025-06-10 00:19:44 +0200

    telescope config

--- lua/config/lazy/telescope.lua
+++ lua/config/lazy/telescope.lua
@@ -5,14 +5,18 @@ return {
 
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-live-grep-args.nvim" ,
+        "nvim-telescope/telescope-live-grep-args.nvim",
     },
 
     config = function()
-        require('telescope').setup({})
-
+        local telescope = require('telescope')
         local builtin = require('telescope.builtin')
+        telescope.setup({})
+
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>ft', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>pww', function()
             local word = vim.fn.expand("<cword>")
@@ -25,10 +29,7 @@ return {
         vim.keymap.set('n', '<leader>sw', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
 
         telescope.load_extension("live_grep_args")
     end
 }
-
-
